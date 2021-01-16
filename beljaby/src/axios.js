250f7679cfde2fb5e1e20036a0893b259699c285
Nam Jaeyoun <siisee111@gmail.com>
2021-01-16 19:06:58 +0900

    Fix name

--- beljaby/src/axios.js
+++ beljaby/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://beljabi.herokuapp.com'
+    baseURL: 'https://beljaby.herokuapp.com'
 })
 
 export default instance
